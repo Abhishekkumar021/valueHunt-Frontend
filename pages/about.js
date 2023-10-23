786dcea69ef77bfef0b67e8153bb57267bced4db
Abhishek Kumar <abhishekkumar232105@gmail.com>
2023-10-23 23:09:35 +0530

    Few New Changes Made

--- pages/about.js
+++ pages/about.js
@@ -14,7 +14,7 @@ export default function About() {
         <div className={styles.about}>
           <h2>About Us</h2>
           <p className={styles.para}>
-            ValueHunt was founded by three friends - Ambrish Kumar, Ayush Kothiyal, and Abhishek Kumar - who shared a passion for leveraging technology to make online shopping more efficient and affordable.After experiencing the frustration of spending hours searching for the best deals on clothes online, the trio realized that there was a need for a platform that could simplify the process and provide users with accurate information about prices and availability.We decided to combine our expertise in artificial intelligence, data analytics, and e-commerce to create ValueHunt.
+            ValueHunt was founded by three friends - Abhishek Kumar, Ayush Kothiyal, and Ambrish Kumar - who shared a passion for leveraging technology to make online shopping more efficient and affordable.After experiencing the frustration of spending hours searching for the best deals on clothes online, the trio realized that there was a need for a platform that could simplify the process and provide users with accurate information about prices and availability.We decided to combine our expertise in artificial intelligence, data analytics, and e-commerce to create ValueHunt.
           </p>
         </div>
       </div>
--- pages/vh.js
+++ pages/vh.js
@@ -1,13 +1,13 @@
-import { useState,useEffect  } from 'react';
+import { useState, useEffect } from 'react';
 
 import Image from 'next/image';
 import Head from 'next/head';
 import styles from '../styles/vh/vh.module.css';
 import res_styles from '../styles/vh/resultPage.module.css';
 import money from '../public/img/money.png'
-let ScrollChange=0;
+let ScrollChange = 0;
 
-export async function getStaticProps() {
+export async function getStaticProps () {
   const token = process.env.token;
   const url = process.env.url;
 
@@ -19,7 +19,7 @@ export async function getStaticProps() {
   };
 }
 
-export default function VhHandler(props) {
+export default function VhHandler (props) {
   const [loading, setLoading] = useState(false);
   const [results, setResults] = useState(null);
   const [error, setError] = useState(null);
@@ -27,11 +27,12 @@ export default function VhHandler(props) {
 
   useEffect(() => {
     // window is accessible here.
-    if(ScrollChange){
-    window.scrollTo({left:0, top:590, behavior: "smooth"});}
+    if (ScrollChange) {
+      window.scrollTo({ left: 0, top: 590, behavior: "smooth" });
+    }
   }, [results]);
 
-  async function submitHandler(event) {
+  async function submitHandler (event) {
     const x = document.getElementById("loader");
     x.classList.remove("close");
     x.classList.add("loading");
@@ -55,18 +56,18 @@ export default function VhHandler(props) {
     setResults(null);
     try {
 
-      const response = await fetch(props.url+'/vh', options);
+      const response = await fetch(props.url + '/vh', options);
 
-     console.log('Resonponse is ', response)
+      console.log('Resonponse is ', response)
       const result = await response.json();
       console.log('+++++++++++++++++++-Result--------------')
       console.log(result)
-      ScrollChange=1;
+      ScrollChange = 1;
       setResults(result);
       setLoading(false);
       x.classList.remove("loading");
       x.classList.add("close");
-      
+
 
     } catch (error) {
       setLoading(false);
@@ -76,7 +77,7 @@ export default function VhHandler(props) {
       setError('Something went wrong. Please try again later');
     }
     setFormKey(formKey + 1);
-   
+
   }
 
   return (
@@ -85,15 +86,15 @@ export default function VhHandler(props) {
         <title>App | ValueHunt</title>
       </Head>
 
-      
+
       <div className={styles.html_form}>
         <div className={styles.heading}>
-      <h1>Upload Cloth Image</h1></div>
+          <h1>Upload Cloth Image</h1></div>
         <form action="#" method="post" onSubmit={submitHandler} className={styles.innerform} key={formKey}>
           <div className={styles.per_info}>
             <div className={styles.cloth}>
               <label htmlFor="Upload photo" className={styles.file}>Choose Picture
-                <input type="file" id="clothImg" name="clothImg" className={styles.clothId} required accept='image/*'/>
+                <input type="file" id="clothImg" name="clothImg" className={styles.clothId} required accept='image/*' />
               </label>
             </div>
 
@@ -120,60 +121,60 @@ export default function VhHandler(props) {
               <>
                 <h2>Results</h2>
                 <div className={res_styles.resultContainer}>
-                {Object.entries(results).map(([site, products]) => (
-                  <div key={site} >
-                    <h3 className={res_styles.siteName}>{site.charAt(0).toUpperCase() + site.slice(1)}</h3>
-                    {Array.isArray(products) ? (products.length === 0 ? (
-                      <div className={res_styles.noProduct}>No products found</div>
-                    ) : (
-                      <div className={res_styles.productContainer}>
-                        {products.map((product, index) => (
-                          <div key={index} className={res_styles.prodContainer}>
-                            <a
-                              href={product.ProdLink}
-                              target="_blank"
-                              rel="noopener noreferrer"
-                            >
-                              <Image
-                                src={product.ImageSrc}
-                                width={200}
-                                height={200}
-                              />
-                              <div className={res_styles.buy_link}>
-                                <a
-                                  href={product.ProdLink}
-                                  target="_blank"
-                                  rel="noopener noreferrer"
-                                >
-                                  {product.Label.substring(0, 20)}
-                                </a>
-                              </div>
-                              <div className={res_styles.price}>
-                                
-                              <Image
-                                src={money}
-                                width={30}
-                                height={30}
-                              />
+                  {Object.entries(results).map(([site, products]) => (
+                    <div key={site} >
+                      <h3 className={res_styles.siteName}>{site.charAt(0).toUpperCase() + site.slice(1)}</h3>
+                      {Array.isArray(products) ? (products.length === 0 ? (
+                        <div className={res_styles.noProduct}>No products found</div>
+                      ) : (
+                        <div className={res_styles.productContainer}>
+                          {products.map((product, index) => (
+                            <div key={index} className={res_styles.prodContainer}>
+                              <a
+                                href={product.ProdLink}
+                                target="_blank"
+                                rel="noopener noreferrer"
+                              >
+                                <Image
+                                  src={product.ImageSrc}
+                                  width={200}
+                                  height={200}
+                                />
+                                <div className={res_styles.buy_link}>
+                                  <a
+                                    href={product.ProdLink}
+                                    target="_blank"
+                                    rel="noopener noreferrer"
+                                  >
+                                    {product.Label.substring(0, 20)}
+                                  </a>
+                                </div>
+                                <div className={res_styles.price}>
 
-                                <h3>{product.Price}</h3></div>
+                                  <Image
+                                    src={money}
+                                    width={30}
+                                    height={30}
+                                  />
 
-                            </a>
+                                  <h3>{product.Price}</h3></div>
 
-                          </div>
-                        ))}
-                      </div>
-                    )) : (
-                      <div className={res_styles.noProduct}>{products}</div>
-                    )}
-                  </div>
-                ))}
+                              </a>
+
+                            </div>
+                          ))}
+                        </div>
+                      )) : (
+                        <div className={res_styles.noProduct}>{products}</div>
+                      )}
+                    </div>
+                  ))}
                 </div>
               </>) : (<div className={res_styles.noProduct}>{results}</div>
             )}
-            
+
           </div>
-          
+
         )}
         {error && (
           <div className={res_styles.error_message}>{error}</div>
